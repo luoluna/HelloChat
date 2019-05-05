09dc53dae0f2e27961c6d433e13ff12886481302
SwevenTears <39357041+SwevenTears@users.noreply.github.com>
2019-05-06 00:06:27 +0800

    creditActivity界面绘制

--- app/src/main/java/com/team/hellochat/activity/CreditActivity.java
+++ app/src/main/java/com/team/hellochat/activity/CreditActivity.java
@@ -1,19 +1,26 @@
 package com.team.hellochat.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.team.hellochat.BaseActivity;
 import com.team.hellochat.R;
+import com.team.hellochat.bean.User;
+import com.team.hellochat.manager.UserManager;
 
 public class CreditActivity extends BaseActivity implements View.OnClickListener {
 
     //top
     private ImageView barBack;
     private TextView barTitle;
+
+    private TextView tvCredit;
+    private TextView tvTips;
+    private RecyclerView recyclerView;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +34,17 @@ public class CreditActivity extends BaseActivity implements View.OnClickListener
     private void bindView() {
         barBack = findViewById(R.id.bar_back);
         barTitle = findViewById(R.id.bar_title);
+
+        tvCredit = findViewById(R.id.tv_credit_point);
+        tvTips = findViewById(R.id.tv_tips);
+        recyclerView = findViewById(R.id.finish_task_up_credit_list);
     }
 
     private void initData() {
         barTitle.setText(R.string.credit_activity_title);
+
+        user = UserManager.getInstance().getUser();
+        tvCredit.setText(String.valueOf(user.getCreditPoint()));
 
         barBack.setOnClickListener(this);
     }
